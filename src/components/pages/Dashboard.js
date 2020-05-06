8e1202e39a151690ff2c832405268719467ac133
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-05 23:21:55 -0600

    login and logout buttons, rendered on homepage, redirects to dashboard

---
+++ src/components/pages/Dashboard.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+
+import Logout from './Logout';
+
+const Dashboard = () => { 
+    const { authState, authService } = useOktaAuth();
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        if (!authState.isAuthenticated) {
+            setUserInfo(null);
+        } else {
+            console.log(authState)
+            authService.getUser().then((info) => {
+                console.log(info)
+                setUserInfo(info);
+        });
+        }
+    }, [authState, authService]);
+  
+    return (
+        <div>
+        { userInfo && 
+            <div>
+            <p>Welcome back, {userInfo.name}!</p>
+            <p></p>
+            </div>
+        }
+        {authState.isAuthenticated && <Logout />}
+        </div>
+    );
+};
+
+export default Dashboard;
