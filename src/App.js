9bda07a100b931ee6782ccc23c295e562e335bbb
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-08 20:27:29 -0600

    fix: adjusted okta config to match okta settings and added SecureRoute for dashboard

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { SecureRoute, LoginCallback } from '@okta/okta-react';
 import OktaConfig from './components/utils/OktaConfig';
-import HomePage from './components/pages/HomePage';
+
 import './App.css';
+
+import HomePage from './components/pages/HomePage';
+import Dashboard from './components/pages/Dashboard';
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
       <Router>
         <Route path="/" exact component={HomePage} />
         <Route path={OktaConfig.callbackPath} component={LoginCallback} />
+        <SecureRoute path='/dashboard' component={Dashboard} />
       </Router>
     </div>
   );
