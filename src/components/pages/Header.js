29daf034ff4ad47461dc87a24f8ef5a4ca673433
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-19 13:14:04 -0600

    Header created

---
+++ src/components/pages/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Login from '../buttons/Login'
+
+const Header = () => {
+
+    return (
+        <header>
+            <h1>Guide Me</h1>
+            <Login />
+        </header>
+    )
+}
