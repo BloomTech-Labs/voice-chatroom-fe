9d8b5477bd8e52185689cbc3648dc82c73498d43
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-27 17:55:48 -0600

    two ul in navbar, second coditionally rendering based on mentorStatus

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+    const [mentorStatus, setMentorStatus] = useState(false)
 
     return (
         <div>
+            <ul className="userNav">
 
+            </ul>
+            <ul className="mentorNav">
+            {mentorStatus ? <li>Become a Mentor</li> : <li></li>}
+            </ul>
+            
         </div>
     )
 }
