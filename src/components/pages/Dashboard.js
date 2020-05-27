ebeef3dc183f230687a233728e89c488214399d7
tsbarrett89 <tsbarrett89@gmail.com>
2020-05-27 17:47:32 -0600

    Navbar.js created and mounted in Dashboard.js

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 
 import Logout from '../buttons/Logout';
+import Navbar from '../navigation/Navbar';
 
 const Dashboard = () => {
     const { authState, authService } = useOktaAuth();
@@ -22,14 +23,17 @@ const Dashboard = () => {
     }, [authState, authService]);
 
     return (
-        <div className="user">
-            {userInfo &&
-                <div>
-                    <p>Welcome back, {userInfo.name}!</p>
-                    <p></p>
-                </div>
-            }
-            {authState.isAuthenticated && <Logout />}
+        <div>
+            <Navbar />
+            <div className="user">
+                {userInfo &&
+                    <div>
+                        <p>Welcome back, {userInfo.name}!</p>
+                        <p></p>
+                    </div>
+                }
+                {authState.isAuthenticated && <Logout />}
+            </div>
         </div>
     );
 };
