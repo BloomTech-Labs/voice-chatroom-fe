f87a9ec0303fe5f4228a6955a8c35dd18bd684af
Leon Chisum <leonchisum3306@gmail.com>
2020-06-04 20:14:03 -0400

    retrieving user from db

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { useOktaAuth } from "@okta/okta-react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 //imported Components
 import Logout from "../buttons/Logout";
@@ -10,17 +10,46 @@ import Navbar from "../navigation/Navbar";
 const Dashboard = () => {
   const { authState, authService } = useOktaAuth();
   const [userInfo, setUserInfo] = useState(null);
+  const [users, setUsers] = useState(null);
 
   useEffect(() => {
     if (!authState.isAuthenticated) {
       setUserInfo(null);
     } else {
       console.log(authState);
+      //set accesstoken and retrieving client and setting it to state
       const accesstoken = authState.accessToken;
       localStorage.setItem("accessToken", accesstoken);
       authService.getUser().then((info) => {
         setUserInfo(info);
       });
+    }
+
+    //creating obj for onboarded user
+    const newUser = {
+      email: userInfo.email,
+      first_name: userInfo.given_name,
+      last_name: userInfo.family_name,
+    };
+
+    //get req to retrieve users in db
+    axiosWithAuth()
+      .get("/users")
+      .then((res) => setUsers(res))
+      .catch((err) => console.log(err));
+
+    //checking to see if onboard user is already in db
+    const existingUser = users.find((i) => i.email === userInfo.email);
+
+    //adding user to db if not already
+    if (!existingUser) {
+      axiosWithAuth()
+        .post("/users", newUser)
+        .then((res) => {
+          console.log(res);
+          setUserInfo(res);
+        })
+        .catch((err) => console.log(err));
     }
   }, [authState, authService]);
 
--- src/components/pages/UserHeader.js
+++ src/components/pages/UserHeader.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function UserHeader(props) {
   return (
     <div>
-      <p>Welcome back, {props.user.name}!</p>
+      <p>Welcome back, {props.user.first_name}!</p>
       <div>
         {/* <img src={props.user.avatar}></img>
         <p>{props.user.location}</p> */}
-        <p>Location: {props.user.zoneinfo}</p>
+        <p>Location: {props.user.location}</p>
       </div>
     </div>
   );
