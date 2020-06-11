12c993d3101a9eadf7d75f9abf9288a5adf02078
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-06-10 20:22:54 -0400

    added calendar rating and interests

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,40 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { useOktaAuth } from '@okta/okta-react';
+import React, { useState, useEffect } from "react";
+import { useOktaAuth } from "@okta/okta-react";
 
-import Logout from '../buttons/Logout';
-import Navbar from '../navigation/Navbar';
+import Logout from "../buttons/Logout";
+import Navbar from "../navigation/Navbar";
+import UserDashboard from "../dashboards/UserDashboard";
 
 const Dashboard = () => {
-    const { authState, authService } = useOktaAuth();
-    const [userInfo, setUserInfo] = useState(null);
+  const { authState, authService } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState(null);
 
-    useEffect(() => {
-        if (!authState.isAuthenticated) {
-            setUserInfo(null);
-        } else {
-            console.log(authState)
-            const accesstoken = authState.accessToken
-            localStorage.setItem('accessToken', accesstoken)
-            authService.getUser().then((info) => {
-                setUserInfo(info);
-            });
-        }
-    }, [authState, authService]);
+  useEffect(() => {
+    if (!authState.isAuthenticated) {
+      setUserInfo(null);
+    } else {
+      console.log(authState);
+      const accesstoken = authState.accessToken;
+      localStorage.setItem("accessToken", accesstoken);
+      authService.getUser().then((info) => {
+        setUserInfo(info);
+      });
+    }
+  }, [authState, authService]);
 
-    return (
-        <div className="dashContainer">
-            <Navbar userInfo={userInfo} />
-            <div className="user">
-                {userInfo &&
-                    <div>
-                        <p>Welcome back, {userInfo.name}!</p>
-                        <p></p>
-                    </div>
-                }
-                {authState.isAuthenticated && <Logout />}
-            </div>
-        </div>
-    );
+  return (
+    <div className="dashContainer">
+      <Navbar userInfo={userInfo} />
+      <div className="user">
+        {userInfo && (
+          <div>
+            <p>Welcome back, {userInfo.name}!</p>
+            <p></p>
+            <UserDashboard />
+          </div>
+        )}
+        {authState.isAuthenticated && <Logout />}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
