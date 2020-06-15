28771c20165d37f4cd7143c8bb5b275fed9a59fd
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-15 16:49:42 -0600

    useForm imported, basic form set up

--- src/components/pages/MentorRegistration.js
+++ src/components/pages/MentorRegistration.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const MentorRegistration = () => {
+    const {register, handleSubmit, watch, errors} = useForm();
+
+    const submitMentorRegistration = (values) => {
+
+    }
 
     return (
-        <form>
-
+        <form onSubmit={handleSubmit(submitMentorRegistration)}>
+            <input
+                name="mentor_name"
+                ref={register({ required: true })}
+            />
+            <input
+                name="category_1"
+                ref={register({ required: true })}
+            />
+            <input
+                name="category_2"
+                ref={register}
+            />
+            <input
+                name="category_3"
+                ref={register}
+            />
+            <input
+                type="submit"
+            />
         </form>
     )
 }
