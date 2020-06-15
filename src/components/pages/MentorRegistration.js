95f28a58d0b999bbc48de2ae19b2a61523c43140
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-15 16:54:10 -0600

    labels added

--- src/components/pages/MentorRegistration.js
+++ src/components/pages/MentorRegistration.js
@@ -10,24 +10,33 @@ const MentorRegistration = () => {
 
     return (
         <form onSubmit={handleSubmit(submitMentorRegistration)}>
-            <input
-                name="mentor_name"
-                ref={register({ required: true })}
-            />
-            <input
-                name="category_1"
-                ref={register({ required: true })}
-            />
-            <input
-                name="category_2"
-                ref={register}
-            />
-            <input
-                name="category_3"
-                ref={register}
-            />
+            <label>Mentor Name:
+                <input
+                    name="mentor_name"
+                    ref={register({ required: true })}
+                />
+            </label>
+            <label>Main Mentor Category:
+                <input
+                    name="category_1"
+                    ref={register({ required: true })}
+                />
+            </label>
+            <label>Secondary Mentor Category:
+                <input
+                    name="category_2"
+                    ref={register}
+                />
+            </label>
+            <label>Third Mentor Category
+                <input
+                    name="category_3"
+                    ref={register}
+                />
+            </label>
             <input
                 type="submit"
+                value="Register"
             />
         </form>
     )
