8f65da3d0c17e5ef7ac7de8d69de3e2022c64a03
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-16 20:40:06 -0600

    imported context and wrapped app in provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { SecureRoute, LoginCallback } from '@okta/okta-react';
 import OktaConfig from './components/utils/OktaConfig';
+
+import { UserContext } from './contexts/UserContext';
 
 import Header from './components/pages/Header';
 import HomePage from './components/pages/HomePage';
@@ -9,16 +11,24 @@ import Dashboard from './components/pages/Dashboard';
 import MentorRegistration from './components/pages/MentorRegistration'
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({})
+
+  const setUser = user => {
+    setCurrentUser(user)
+  }
+
   return (
-    <div className="App">
-      <Header />
-      <Router>
-        <Route path="/" exact component={HomePage} />
-        <Route path={OktaConfig.callbackPath} component={LoginCallback} />
-        <SecureRoute path='/dashboard' component={Dashboard} />
-        <Route path='/mentor-registration' component={MentorRegistration} />
-      </Router>
-    </div>
+    <UserContext.Provider value={{ currentUser, setUser }}>
+      <div className="App">
+        <Header />
+        <Router>
+          <Route path="/" exact component={HomePage} />
+          <Route path={OktaConfig.callbackPath} component={LoginCallback} />
+          <SecureRoute path='/dashboard' component={Dashboard} />
+          <Route path='/mentor-registration' component={MentorRegistration} />
+        </Router>
+      </div>
+    </UserContext.Provider>
   );
 }
 
