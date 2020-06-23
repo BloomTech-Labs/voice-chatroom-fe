0de217126199d9f018840a62f5f6445ece0a7bcf
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-23 09:27:03 -0600

    context created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { SecureRoute, LoginCallback } from '@okta/okta-react';
 import OktaConfig from './components/utils/OktaConfig';
