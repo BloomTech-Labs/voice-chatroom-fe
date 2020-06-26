4772ddc6185d205013b646a051a19919ee76c564
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-25 22:55:35 -0600

    created setUser action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,1 +1,8 @@
 import { userConstants } from '../actions/types';
+
+export const setUser = (user) => dispatch => {
+    dispatch({
+        type: userConstants.LOGIN_SUCCESS,
+        payload: user
+    })
+}
