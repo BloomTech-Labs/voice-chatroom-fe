e444732343c69637744153e42b59c803671a4e58
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-25 23:13:04 -0600

    changed props to redux state

--- src/components/dashboards/UserHeader.js
+++ src/components/dashboards/UserHeader.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from 'react-redux'
 
 export default function UserHeader(props) {
   const [isEditing, setEditing] = useState(false);
+  const currentUser = useSelector(state => state.authReducer.user)
 
   const onChanges = (e) => {
     
@@ -11,11 +13,11 @@ export default function UserHeader(props) {
     <div>
       <form>
         {/* <input value={props.user.name} type="text" onChange={}/> */}
-        <p>Welcome back, {props.user.name}!</p>
+        <p>Welcome back, {currentUser.given_name} {currentUser.family_name}!</p>
         <div>
           {/* <img src={props.user.avatar}></img>
         <p>{props.user.location}</p> */}
-          <p>Location: {props.user.location}</p>
+          <p>Location: {currentUser.location}</p>
         </div>
       </form>
     </div>
