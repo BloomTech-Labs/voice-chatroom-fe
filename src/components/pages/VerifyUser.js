199e74bd89028a224fd6790b63430526ffd5ec68
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-25 22:59:16 -0600

    removed context

--- src/components/pages/VerifyUser.js
+++ src/components/pages/VerifyUser.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { useOktaAuth } from "@okta/okta-react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from 'react-redux'
