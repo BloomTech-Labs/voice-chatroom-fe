fed4c2665a6d57229e25c01a857ee8cb4b6509b7
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-26 22:03:59 -0600

    removed unused code

--- src/components/pages/VerifyUser.js
+++ src/components/pages/VerifyUser.js
@@ -1,58 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useOktaAuth } from "@okta/okta-react";
-import { useHistory } from "react-router-dom";
 import { useDispatch } from 'react-redux'
 
-import { setUser, userLoginOrRegister } from '../../actions/auth'
-import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { userLoginOrRegister } from '../../actions/auth'
 
 const VerifyUser = () => {
     const { authState, authService } = useOktaAuth();
-    const [currentUser, setCurrentUser] = useState({id: 0})
     const dispatch = useDispatch()
-    let history = useHistory();
 
     useEffect(() => {
         dispatch(userLoginOrRegister(authState, authService))
-        // if (!authState.isAuthenticated){
-        // } else {
-        //     const accessToken = authState.accessToken;
-        //     localStorage.setItem("accessToken", accessToken);
-        //     authService.getUser().then((info) => {
-        //         axiosWithAuth()
-        //             .post('users/email', {email: info.email})
-        //             .then(res => {
-        //                 if(res.data.length > 0){
-        //                     setCurrentUser(res.data[0])
-        //                     // history.push('/user-dashboard')
-        //                 } else {
-        //                     axiosWithAuth()
-        //                         .post('users/', {
-        //                             email: info.email,
-        //                             given_name: info.given_name,
-        //                             family_name: info.family_name
-        //                         })
-        //                         .then(res => {
-        //                             setCurrentUser(res.data[0])
-        //                             // history.push('/user-dashboard')
-        //                         })
-        //                         .catch(err => console.log(err.response))
-        //                     }
-        //             })
-        //             .catch(error => {
-        //                 console.log('this is the other one', error)
-        //             })
-        //     })
-        // }
     }, [])
-
-    // useEffect(() => {
-    //     console.log(currentUser)
-    //     dispatch(setUser(currentUser))
-    //     if(currentUser.id > 0){
-    //         history.push('/user-dashboard')
-    //     }
-    // }, [currentUser])
 
   return (
     <div>
