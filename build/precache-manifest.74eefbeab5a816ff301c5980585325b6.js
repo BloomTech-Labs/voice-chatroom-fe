faaa6215df61ee233aad5f6bd01f59318b96b609
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-06 19:01:35 -0600

    added build

---
+++ build/precache-manifest.74eefbeab5a816ff301c5980585325b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2128975c1ea1eb612a53269c8b50d048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b258e596faab4556ddc",
+    "url": "/static/css/main.7cccaa9d.chunk.css"
+  },
+  {
+    "revision": "25efac8e4b1db318bb2e",
+    "url": "/static/js/2.add8199b.chunk.js"
+  },
+  {
+    "revision": "980de3696e4ffeb56eee5888e9466e1c",
+    "url": "/static/js/2.add8199b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b258e596faab4556ddc",
+    "url": "/static/js/main.c9e6bf5e.chunk.js"
+  },
+  {
+    "revision": "3531b2f2313e97b0fe19",
+    "url": "/static/js/runtime-main.6213a6a6.js"
+  },
+  {
+    "revision": "2f8326be2e971edd5df6bec737d69f5a",
+    "url": "/static/media/climb.2f8326be.jpg"
+  },
+  {
+    "revision": "588b1a3dbc0b92f53f411b0baf22e36b",
+    "url": "/static/media/mentorpair.588b1a3d.png"
+  },
+  {
+    "revision": "b914966f4aeefcffcd6a4469b02919c0",
+    "url": "/static/media/plus.b914966f.png"
+  },
+  {
+    "revision": "b8a8779bfd5f0b42cb02896a534f4fff",
+    "url": "/static/media/thread.b8a8779b.jpg"
+  }
+]);
