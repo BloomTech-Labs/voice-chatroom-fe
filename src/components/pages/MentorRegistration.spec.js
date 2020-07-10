73c92a03e13daf963920ebbcfd1ef066d9f3dcaf
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-09 19:25:51 -0600

    troubleshooting enzyme issue

--- src/components/pages/MentorRegistration.spec.js
+++ src/components/pages/MentorRegistration.spec.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import MentorRegistration from './MentorRegistration'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
 
 test("renders mentor registration form", async () => {
     const { getByText } = render(<MentorRegistration />);
