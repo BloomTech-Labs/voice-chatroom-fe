78c3cc50887d29b419feaa38fa569d3df0209942
tsbarrett89 <tsbarrett89@gmail.com>
2020-07-09 21:35:24 -0600

    added one test

---
+++ src/components/pages/Dashboard.spec.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Dashboard from './Dashboard'
+
+configure({ adapter: new Adapter() })
+
+describe('<Dashboard />', () => {
+    test('Dashboard renders', () => {
+        const wrapper = shallow(<Dashboard />)
+        console.log(wrapper.debug())
+    })
+})
