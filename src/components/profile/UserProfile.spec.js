d3e8563f34fa1c7278d0c150e67b096c2df17edf
Tony <tonyrkovar@gmail.com>
2020-07-13 20:37:04 -0500

    chore: updating package json and test file for code climate

--- src/components/profile/UserProfile.spec.js
+++ src/components/profile/UserProfile.spec.js
@@ -5,32 +5,57 @@ import { render, fireEvent } from '@testing-library/react';
 import Adapter from 'enzyme-adapter-react-16';
 import UserProfile from './UserProfile'
 
+import { createStore } from 'redux'
+import authReducer from '../../reducers/auth'
+import { Provider } from 'react-redux'
+
 import 'mutationobserver-shim';
 global.MutationObserver = window.MutationObserver;
 
+const currentUser = {
+  id: 1,
+  given_name: "",
+  user_bio: "",
+  email: "",
+  family_name: "",
+  username: "",
+  location: "",
+  interest_1: ""
+}
+
+
+function renderWithRedux(
+  ui,
+  { initialState, store = createStore(authReducer, initialState) } = {}
+) {
+  return {
+    ...mount(<Provider store={store}>{ui}</Provider>), store
+  }
+}
+
 configure({ adapter: new Adapter() });
 
-let wrapper;
+const wrapper = ""
 
-beforeEach( () =>{
-  wrapper = mount(<UserProfile />)
+beforeEach(() => {
+  wrapper = renderWithRedux(<UserProfile />)
 });
 
-describe('User profile renders without crashing', () =>{
-    it('renders without crashing', () =>{
-        mount(<UserProfile />);
-    });
+describe('User profile renders without crashing', () => {
+  it('renders without crashing', () => {
+    mount(<UserProfile />);
   });
-  
+});
 
-  it('Should render 7 <label>s', () =>{
-    expect(wrapper.find('label')).toHaveLength(7)
-  });
 
-  it('Should render 1 <form>', () =>{
-    expect(wrapper.find('form')).toHaveLength(1)
-  });
+it('Should render 7 <label>s', () => {
+  expect(wrapper.find('label')).toHaveLength(7)
+});
 
-  it('Should render 6 <input>s', () =>{
-    expect(wrapper.find('input')).toHaveLength(6)
-  });
+it('Should render 1 <form>', () => {
+  expect(wrapper.find('form')).toHaveLength(1)
+});
+
+it('Should render 6 <input>s', () => {
+  expect(wrapper.find('input')).toHaveLength(6)
+});
