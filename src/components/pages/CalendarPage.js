fb7c8653c035e2dfacf2ceacbc72b8086f0e2f73
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-07-18 02:21:42 -0400

    Added Big Calendar, to make Tony proud

--- src/components/pages/CalendarPage.js
+++ src/components/pages/CalendarPage.js
@@ -2,14 +2,29 @@ import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
 function CalendarPage() {
   const [date, setDate] = useState(new Date());
+  const [events, setEvents] = useState([]);
 
   const onChange = (date) => {
     setDate(date);
   };
 
-  return <div className="calendar-container"></div>;
+  return (
+    <div className="calendar-container">
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 800, width: 1000 }}
+      />
+    </div>
+  );
 }
 
 export default CalendarPage;
