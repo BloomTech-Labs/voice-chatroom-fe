eb17fb43d5761b67a76e3bbf34e33fb4ac8205e2
Leon Chisum <leonchisum3306@gmail.com>
2020-07-21 20:27:04 -0400

    test added for auth reducer login success

--- src/reducers/authReducer.spec.js
+++ src/reducers/authReducer.spec.js
@@ -2,6 +2,7 @@ import { userConstants } from "../actions/types";
 import authReducer from "./auth";
 
 describe("Auth Reducer", () => {
+
   it("should return default state", () => {
     const newState = authReducer(undefined, {});
     expect(newState).toEqual({
@@ -27,4 +28,30 @@ describe("Auth Reducer", () => {
       error: null,
     });
   });
+
+  it('should return new user if correct type received', () => {
+      const user =  {
+        id: 0,
+        email: "test@test.com",
+        given_name: "test",
+        family_name: "user",
+        username: "testy",
+        location: "testing center",
+        interest_1: "take test",
+        interest_2: "",
+        interest_3: "",
+        created_at: "",
+        avatar: "",
+        isMentor: false,
+        user_bio: "",
+        user_rating: 0,
+      }
+
+      const newState = authReducer(undefined, {
+          type: userConstants.LOGIN_SUCCESS,
+          payload: user
+      })
+
+      expect(newState.user).toEqual(user)
+  })
 });
