b9182a24ad9b9617709fe7960e8f9da8a41751f1
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-07-22 20:52:27 -0400

    User action creator tests

--- src/actions/users.spec.js
+++ src/actions/users.spec.js
@@ -1,13 +1,44 @@
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
 import * as actions from "./users";
 import * as types from "./users";
+import fetchMock from "fetch-mock";
 
-describe("actions", () => {
-  it("should create an action to get users", () => {
-    const user = [];
-    const expectedAction = {
-      type: types.FETCH_USER_SUCCESS,
-      user,
-    };
-    expect(actions.getAllUsers(user)).toMatchObject(expectedAction);
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe("async actions", () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it("creates FETCH_ALL_SUCCESS when fetching users has been done", () => {
+    fetchMock.getOnce("/users", {
+      body: [
+        { given_name: "bob", family_name: "shultz" },
+        { given_name: "rick", family_name: "smitts" },
+        { given_name: "duke", family_name: "nukem" },
+      ],
+      headers: { "content-type": "application/json" },
+    });
+
+    const expectedActions = [
+      { type: types.FETCH_ALL_REQUEST },
+      {
+        type: types.FETCH_ALL_SUCCESS,
+        body: {
+          users: [
+            { given_name: "bob", family_name: "shultz" },
+            { given_name: "rick", family_name: "smitts" },
+            { given_name: "duke", family_name: "nukem" },
+          ],
+        },
+      },
+    ];
+    const store = mockStore({ users: [] });
+
+    return store.dispatch(actions.getAllUsers()).then(() => {
+      expect(store.getAllUsers()).toEqual(expectedActions);
+    });
   });
 });
